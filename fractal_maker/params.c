19d1d1155fa6ef3702cfa3c96660cfec02d90bd9
Abdullah İbrahim Durusoy <99260593+marasama@users.noreply.github.com>
2023-10-12 12:21:55 +0300

    First time

---
+++ fractal_maker/params.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   params.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adurusoy <adurusoy@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 13:13:19 by fvieira           #+#    #+#             */
+/*   Updated: 2023/10/12 09:03:25 by adurusoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	mandelbrot_param(t_fractal *fractal, char *name)
+{
+	fractal->width = 1280;
+	fractal->height = 720;
+	fractal->iterations = 150;
+	fractal->zoom = 1;
+	fractal->x = 0;
+	fractal->y = 0;
+	fractal->xarrow = 0;
+	fractal->yarrow = 300;
+	fractal->color = 0xccf1ff;
+	fractal->name = name;
+}
+
+void	julia_param(t_fractal *fractal, char *name)
+{
+	fractal->width = 1280;
+	fractal->height = 720;
+	fractal->c.x = -0.7;
+	fractal->c.y = 0.27015;
+	fractal->radius = 3;
+	fractal->iterations = 150;
+	fractal->zoom = 1;
+	fractal->x = 0;
+	fractal->y = 0;
+	fractal->xarrow = 0;
+	fractal->yarrow = 0;
+	fractal->color = 0xccf1ff;
+	fractal->name = name;
+}
+
+void	burning_param(t_fractal *fractal, char *name)
+{
+	fractal->width = 1280;
+	fractal->height = 720;
+	fractal->iterations = 150;
+	fractal->zoom = 8.157307;
+	fractal->x = 0;
+	fractal->y = 0;
+	fractal->xarrow = 1710.0;
+	fractal->yarrow = 4530.0;
+	fractal->color = 0xccf1ff;
+	fractal->name = name;
+}
